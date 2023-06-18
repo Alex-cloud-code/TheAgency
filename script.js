a8ac3a05c60a67dc0b6ee144d1a1016e9893d824
Alexander Kichigin <alex-kichigin@yandex.ru>
2023-06-18 11:45:56 +0300

    second commit

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ const burgerEl = document.querySelector('.burger-menu');
 const menuEl = document.querySelector('.menu');
 const headerEl = document.querySelector('.header_burger-style');
 const linkEl = document.querySelector('.menu__link');
+const closureEl = document.querySelector('.closure');
 
 burgerEl.addEventListener('click', function () {
   menuEl.classList.add('menu_vivsible');
+  closureEl.classList.add('closure_visible');
   burgerEl.classList.remove('burger-menu');
   headerEl.classList.remove('header_burger-style');
 });
 
 menuEl.addEventListener('click', function () {
     menuEl.classList.remove('menu_vivsible');
+    closureEl.classList.remove('closure_visible');
     burgerEl.classList.add('burger-menu');
     headerEl.classList.add('header_burger-style');
   });
